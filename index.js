ff43a502f70d3bdf4a4a3bd8c08bf35a712e3d8d
Idan Refaeli <idanref@gmail.com>
2022-08-25 18:29:13 +0300

    Deployment Preperation

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
-const app = require('./app');
+const path = require('path');
+const express = require('express');
+require('./db/mongoose');
+const workshopRouter = require('./routers/workshop');
+const adminInfoRouter = require('./routers/adminInfo');
+const couponRouter = require('./routers/coupon');
+// const commentRouter = require('./routers/comment');
+
+const app = express();
+
+// Each app.use(middleware) is called every time a request is sent to the server.
+
+app.use(express.json());
+app.use(workshopRouter);
+app.use(adminInfoRouter);
+app.use(couponRouter);
+// app.use(commentRouter); // comments are not used in admin interface for now
+
+// Serving React app on {port}
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/client/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.send('API Running!');
+  });
+}
+
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
